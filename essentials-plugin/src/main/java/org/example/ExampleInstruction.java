01c6bd553085ad78b950742fd716b6291627d2c0
Kenan Salic <kenan.salic@bloomreach.com>
2021-05-03 15:30:27 +0200

    example instruction

--- essentials-plugin/src/main/java/org/example/ExampleInstruction.java
+++ essentials-plugin/src/main/java/org/example/ExampleInstruction.java
@@ -4,11 +4,17 @@ import java.util.Map;
 import java.util.function.BiConsumer;
 
 import org.onehippo.cms7.essentials.sdk.api.install.Instruction;
+import org.onehippo.cms7.essentials.sdk.api.service.JcrService;
+
+import javax.jcr.Session;
 
 /**
  * ExampleInstruction
  */
 public class ExampleInstruction implements Instruction {
+
+    @javax.inject.Inject
+    private JcrService jcrService;
 
     @Override
     public Status execute(Map<String, Object> parameters) {
@@ -17,6 +23,8 @@ public class ExampleInstruction implements Instruction {
 
     @Override
     public void populateChangeMessages(final BiConsumer<Type, String> changeMessageQueue) {
-        changeMessageQueue.accept(Type.EXECUTE, "Example instruction change message");
+        Session session = jcrService.createSession();
+        String userID = session.getUserID();
+        changeMessageQueue.accept(Type.EXECUTE, "Example instruction change message " + userID);
     }
 }
